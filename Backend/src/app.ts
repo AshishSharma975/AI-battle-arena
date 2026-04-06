29a15422ace651f46e0daef26af81011cfb4ad86
AshishSharma975 <ashish8389sharma@gmail.com>
2026-04-07 00:39:01 +0530

    fixing

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -53,10 +53,9 @@ app.post("/invoke", async (req, res) => {
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+app.use(express.static(path.join(__dirname, "../../Frontend/dist")));
 
-app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "../../Frontend/dist/index.html"));
 });
-
 export default app;

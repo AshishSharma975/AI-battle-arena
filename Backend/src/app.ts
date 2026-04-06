9c283202f26099a37494abf66de41ce80c0662c5
AshishSharma975 <ashish8389sharma@gmail.com>
2026-04-07 00:10:08 +0530

    changes

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -2,14 +2,16 @@ import express from "express";
 import runGraph from "./ai/graph.ai.js";
 import { success } from "zod";
 import cors from "cors";
-
+import path from "path";
 
 const app = express();
+
 app.use(express.json());
+
 app.use(cors({
-    origin:"http://localhost:5173",
-    methods:["GET","POST"],
-    credentials:true
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true
 }));
 
 
@@ -18,14 +20,27 @@ app.get("/use-me", async (req, res) => {
     res.json(result);
 });
 
-app.post("/invoke",async(req,res)=>{
-    const {problem}=req.body;
+app.post("/invoke", async (req, res) => {
+    const { problem } = req.body;
     const result = await runGraph(problem);
     res.status(200).json({
-        message:"graph invoked successfully",
-        success:true,
-        data:result
+        message: "graph invoked successfully",
+        success: true,
+        data: result
     });
-})
+});
 
-export default app
+
+
+// ES module me __dirname fix
+const __dirname = new URL('.', import.meta.url).pathname;
+
+// static files serve
+app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+
+// React routes handle
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
+});
+
+export default app;

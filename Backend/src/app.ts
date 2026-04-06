7604a8b90068f77a1d7eda46e99f1c5f2c743dae
AshishSharma975 <ashish8389sharma@gmail.com>
2026-04-07 00:35:13 +0530

    fix

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -55,7 +55,7 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
 });
 

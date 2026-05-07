30f3233fc20f29bb216c7d0d60cda452b99a7da0
AshishSharma975 <ashish8389sharma@gmail.com>
2026-05-07 19:35:10 +0530

    feat: initialize Gemini, Mistral, and Cohere chat models using LangChain adapters

--- Backend/src/ai/model.ai.ts
+++ Backend/src/ai/model.ai.ts
@@ -9,12 +9,15 @@ const GeminiModel = new ChatGoogle({
   temperature: 0.7,
   apiKey: config.geminiApiKey as string,
 });
+
+
 const MistralModel = new ChatMistralAI({
     model: "mistral-medium-latest",
     maxTokens: 2048,
     temperature: 0.7,
     apiKey: config.mistralApiKey as string,
 });
+
 
 const CohereModel = new ChatCohere({
     model: "command-a-03-2025",
